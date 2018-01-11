6ac183676ddacc267965c199e376754e05e8d377
Sebastian Jylanki <sebastian.jylanki@gmail.com>
2018-01-11 19:43:30 +0700

    Multiple changes
    - added input sanitizing logic and a sanitizer for all MD CTL commands
    - refactored code
    - using more commands from zmapi library

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ import json
 import argparse
 from zmapi.codes import error
 from zmapi.zmq import SockRecvPublisher
+from zmapi.zmq.utils import ident_to_str, split_message
 from zmapi.logging import setup_root_logger
+from zmapi.exceptions import *
 import uuid
 from time import time, gmtime
 from datetime import datetime
@@ -18,19 +20,11 @@ from pprint import pprint, pformat
 
 MODULE_NAME = "frontend"
 
-################################# EXCEPTIONS ##################################
-
-class CodecException(Exception):
-    pass
-
-class DecodingException(Exception):
-    pass
-
-class InvalidArguments(Exception):
-    pass
-
-class CommandNotImplemented(Exception):
-    pass
+EXCEPTION_TO_ECODE = {
+    DecodingException: error.DECODE,
+    CodecException: error.CODEC,
+    InvalidArgumentsException: error.ARGS,
+}
 
 ################################### GLOBALS ###################################
 
@@ -46,86 +40,117 @@ g.num_messages_handled = 0
 g.num_msg_ids_added = 0
 g.num_invalid_messages = 0
 
-################################### HELPERS ###################################
-
-def ident_to_str(ident):
-    return "/".join([x.decode("latin-1").replace("/", "\/") for x in ident])
-
 ###############################################################################
 
-async def run_ctl_interceptor():
-    L.info("run_ctl_interceptor running ...")
-    while True:
-        msg_parts = await g.sock_ctl.recv_multipart()
-        try:
-            ident, msg = split_message(msg_parts)
-        except ValueError as err:
-            L.error(str(err))
-            g.num_invalid_messages += 1
-            continue
-        if len(msg) == 0:
-            # handle ping message
-            await g.sock_deal.send_multipart(msg_parts)
-            res = await g.sock_deal_pub.poll_for_pong()
-            await g.sock_ctl.send_multipart(res)
-            continue
-        create_task(handle_msg_1(ident, msg))
+PYTHON_TYPE_TO_ZMAPI_TYPE = {
+    bool: "bool",
+    int: "int",
+    float: "float",
+    str: "str",
+    list: "list",
+    dict: "record",
+}
 
-def split_message(msg_parts):
-    separator_idx = None
-    for i, part in enumerate(msg_parts):
-        if not part:
-            separator_idx = i
-            break
-    if not separator_idx:
-        raise ValueError("ident separator not found")
-    ident = msg_parts[:separator_idx]
-    msg = msg_parts[separator_idx+1]
-    return ident, msg
+def check_spec(d, spec, optional):
+    name = spec[0]
+    dtype = spec[1]
+    vmin = None
+    vmax = None
+    if len(spec) == 4:
+        vmin = spec[2]
+        vmax = spec[3]
+    val = d.get(name, None)
+    if optional and val is None:
+        return
+    if val is None:
+        raise InvalidArgumentsException("missing field: {}".format(name))
+    if not isinstance(val, dtype):
+        raise InvalidArgumentsException(
+                "field '{}' has wrong type, expected type: {}"
+                .format(name, PYTHON_TYPE_TO_ZMAPI_TYPE[dtype]))
+    if vmin is not None:
+        if val < vmin:
+            raise InvalidArgumentsException(
+                    "field '{}' must have a value greater than {}"
+                    .format(name, vmin))
+    if vmax is not None:
+        if val > vmax:
+            raise InvalidArgumentsException(
+                    "field '{}' must have a value less than {}"
+                    .format(name, vmax))
 
-async def handle_msg_1(ident, raw_msg):
-    g.num_messages_handled += 1
-    try:
-        msg = parse_message(raw_msg)
-        if "msg_id" not in msg:
-            msg["msg_id"] = generate_msg_id()
-            g.num_msg_ids_added += 1
-        else:
-            msg["msg_id"] = str(msg["msg_id"])
-        res = await handle_msg_2(ident, msg)
-    except DecodingException as e:
-        L.exception("decoding exception:")
-        await send_error(ident, msg["msg_id"], error.DECODE, str(e))
-    except CodecException as e:
-        L.exception("codec exception:")
-        await send_error(ident, msg["msg_id"], error.CODEC)
-    except InvalidArguments as e:
-        L.exception("invalid arguments on message:")
-        await send_error(ident, msg["msg_id"], error.ARGS, str(e))
+def check_content(d, req_specs, opt_specs):
+    for spec in req_specs:
+        check_spec(d, spec, False)
+    for spec in opt_specs:
+        check_spec(d, spec, True)
 
-async def send_error(ident, msg_id, ecode, msg=None):
+# sanitize_* can be specs argument for check_content function or function
+# defining the arguments checking procedure.
+
+sanitize_subscribe = [
+    [("ticker_id", str),
+    ("order_book_speed", int, 0, 10),
+    ("trades_speed", int, 0, 10),
+    ("order_book_levels", int, 0, None),
+    ("emit_quotes", bool)]
+]
+
+sanitize_unsubscribe = [[("ticker_id", str)]]
+
+sanitize_get_ticker_info = [("ticker", dict)]
+
+sanitize_get_snapshot = [
+    [("ticker_id", str)],
+    [("daily_data", bool), ("order_book_levels", int, 0, None)]
+]
+
+sanitize_list_directory = [[("directory", str)]]
+
+SANITIZERS = {}
+def populate_sanitizers():
+    for k in list(globals()):
+        if k.startswith("sanitize_"):
+            cmd = k[9:]
+            v = globals()[k]
+            if callable(v):
+                SANITIZERS[cmd] = v
+            else:
+                if len(v) == 2:
+                    req = v[0] if v[0] is not None else []
+                    opt = v[1] if v[1] is not None else []
+                elif len(v) == 1:
+                    req = v[0]
+                    opt = []
+                else:
+                    raise Exception("incorrect sanitizer specs definition")
+                f = lambda x, req=req, opt=opt: \
+                        check_content(x["content"], req, opt)
+                SANITIZERS[cmd] = f
+populate_sanitizers()
+            
+###############################################################################
+
+async def send_error(ident, msg_id, ecode, what=None):
     g.num_invalid_messages += 1
-    msg = error.gen_error(ecode, msg)
+    if isinstance(what, Exception):
+        what = "{}: {}".format(type(what).__name__, what)
+    msg = error.gen_error(ecode, what)
     msg["msg_id"] = msg_id
     msg = " " + json.dumps(msg)
     msg = msg.encode()
     await g.sock_ctl.send_multipart(ident + [b"", msg])
 
-def parse_message(msg):
-    codec = int(msg[0])
-    if codec != 32:
-        raise CodecException()
-    try:
-        msg = msg.decode()
-        msg = json.loads(msg)
-    except Exception as e:
-        raise DecodingException(e)
-    return msg
+def sanitize_msg(msg):
+    sanitizer = SANITIZERS.get(msg["command"])
+    if sanitizer:
+        sanitizer(msg)
 
 async def handle_msg_2(ident, msg):
     cmd = msg.get("command")
     if not cmd:
         raise InvalidArguments("command missing")
+    sanitize_msg(msg)
     debug_str = "ident={}, command={}, msg_id={}"
     debug_str = debug_str.format(ident_to_str(ident), cmd, msg["msg_id"])
     L.debug("> " + debug_str)
@@ -148,8 +173,52 @@ async def handle_msg_2(ident, msg):
     L.debug("< " + debug_str)
     await g.sock_ctl.send_multipart(msg_parts)
 
-def generate_msg_id():
-    return str(uuid.uuid4())
+
+def parse_message(msg):
+    codec = int(msg[0])
+    if codec != 32:
+        raise CodecException()
+    try:
+        msg = msg.decode()
+        msg = json.loads(msg)
+    except Exception as e:
+        raise DecodingException(e)
+    return msg
+
+async def handle_msg_1(ident, raw_msg):
+    g.num_messages_handled += 1
+    try:
+        msg = parse_message(raw_msg)
+        if "msg_id" not in msg:
+            msg["msg_id"] = str(uuid.uuid4())
+            g.num_msg_ids_added += 1
+        else:
+            msg["msg_id"] = str(msg["msg_id"])
+        msg_id = msg["msg_id"]
+        res = await handle_msg_2(ident, msg)
+    except Exception as e:
+        L.exception("{} on msg_id: {}".format(type(e).__name__, msg_id))
+        ecode = EXCEPTION_TO_ECODE.get(type(e), error.GENERIC)
+        await send_error(ident, msg_id, ecode,
+                         e if ecode == error.GENERIC else str(e))
+
+async def run_ctl_interceptor():
+    L.info("run_ctl_interceptor running ...")
+    while True:
+        msg_parts = await g.sock_ctl.recv_multipart()
+        try:
+            ident, msg = split_message(msg_parts)
+        except ValueError as err:
+            L.error(str(err))
+            g.num_invalid_messages += 1
+            continue
+        if len(msg) == 0:
+            # handle ping message
+            await g.sock_deal.send_multipart(msg_parts)
+            res = await g.sock_deal_pub.poll_for_pong()
+            await g.sock_ctl.send_multipart(res)
+            continue
+        create_task(handle_msg_1(ident, msg))
 
 ###############################################################################
 
